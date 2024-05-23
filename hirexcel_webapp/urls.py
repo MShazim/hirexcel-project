2bcf5c5e821172164021dd0e1b703479f6913a49
Muhammad Shazim <aslamshazim20@gmail.com>
2024-05-23 22:15:05 +0500

    added some pages

--- hirexcel_webapp/urls.py
+++ hirexcel_webapp/urls.py
@@ -4,5 +4,10 @@ from hirexcel_webapp import views
 
 urlpatterns = [
     path('', views.start_screen, name='start_screen'),
-    path('login', views.login, name='login')
+    path('jobseeker_login', views.jobseeker_login, name='jobseeker_login'),
+    path('recruiter_login', views.recruiter_login, name='recruiter_login'),
+    path('jobseeker_create_account', views.jobseeker_create_account, name='jobseeker_create_account'),
+    path('recruiter_create_account', views.recruiter_create_account, name='recruiter_create_account'),
+    path('jobseeker_home', views.jobseeker_home, name='jobseeker_home'),
+    path('recruiter_home', views.recruiter_home, name='recruiter_home'),
 ]
--- hirexcel_webapp/views.py
+++ hirexcel_webapp/views.py
@@ -4,5 +4,20 @@ from django.shortcuts import render
 def start_screen(request):
     return render(request, './start-screen/start_screen.html')
 
-def login(request):
-    return render(request, './login/login.html')
+def jobseeker_login(request):
+    return render(request, './login/jobseeker_login.html')
+
+def recruiter_login(request):
+    return render(request, './login/recruiter_login.html')
+
+def jobseeker_create_account(request):
+    return render(request, './create_account/jobseeker_create_account.html')
+
+def recruiter_create_account(request):
+    return render(request, './create_account/recruiter_create_account.html')
+
+def jobseeker_home(request):
+    return render(request, './home/jobseeker_home.html')
+
+def recruiter_home(request):
+    return render(request, './home/recruiter_home.html')
